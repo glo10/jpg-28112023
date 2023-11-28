65d761e5aa34bc8a2391a8752858853ebf516de4
formateur-glodiee-tshimini <contact@tshimini.fr>
2023-11-28 14:24:15 +0100

    refactor: add comments

--- demo/npm/form-component.js
+++ demo/npm/form-component.js
@@ -1,6 +1,7 @@
 class FormComponent extends HTMLElement {
   constructor () {
     super()
+    // Contenu HTML de mon composant
     this.innerHTML = ` 
     <form action="">
       <input type="text" placeholder="nom">
@@ -10,27 +11,45 @@ class FormComponent extends HTMLElement {
     `
   }
 
+  /**
+   * S'execute lorsque l'élément est ajouté dans le DOM
+   */
   connectedCallback () {
     this.onFocus()
     this.onBlur()
   }
 
+  /**
+   * S'exécute lorsque l'élément est rétiré du DOM
+   */
   disconnectedCallback () {
     console.log('disconnected')
   }
 
+  /**
+   * Fonction classique qui effectue des opérations
+   * Cette fonction est appelé dans la fonction connectedCallback
+   */
   onBlur () {
     console.log('blur')
   }
 
+  /**
+   * Idem que onBlur()
+   */
   onFocus () {
+    // Récupération de tous les inputs de type text
+    // querySelectorAll renvoie un tableau d'ou la nécessité de parcours ce tableau à l'aide d'une boucle
     const inputs = this.querySelectorAll('input[type=text]')
     for (let i = 0; i < inputs.length; i++) {
+      // Ajout/greffer un événement focus pour chaque input
       inputs[i].addEventListener('focus', (e) => {
         console.log('focus', e.target.value)
       })
     }
   }
 }
+// Déterminer la balise associé à notre CustomElement en respectant la convention de nommage kebab-case
 customElements.define('my-form', FormComponent)
+// Export pour pouvoir l'utiliser ailleurs dans d'autres scripts
 export default FormComponent
--- demo/npm/index.js
+++ demo/npm/index.js
@@ -1,5 +1,6 @@
 import FormComponent from './form-component.js'
 
+// Ajout dynamique du customElement avec la logique (comportement géré depuis la classe du CustomElement)
 document.querySelector('button').addEventListener('click', () => {
   const newForm = new FormComponent()
   document.querySelector('body').append(newForm)
