8d0e0edc6c254835707662ffdf352a05ba88ba5f
formateur-glodiee-tshimini <contact@tshimini.fr>
2023-11-30 09:51:21 +0100

    refactor: demo JQuery Vanilla and concat

--- demo/regex/index.mjs
+++ demo/regex/index.mjs
@@ -29,4 +29,29 @@ $(function() {
   $('input').on('keyup', function () {
     console.log('check', $(this).check())
   })
+
+  const name = 'nom'
+  /**
+   * On peut mélanger JQuery et VanillaJS (JavaScript natif)
+   * JQuery est une façade (design patter)
+   * qui permet de faire les mêmes choses que JS de façon simplifiée
+   * Donc JQuery c'est du JS et les 2 notations peuvent être utilisé dans votre code
+   */
+  // Concaténation avec ` et ${nomVariable}` ${} va récupéré la valeur de la variable
+  $(`input[name=${name}]`).on('keydown', function(e) {
+    const p = document.querySelector('p')
+    /**
+     * avec $('p') on récupère un objet
+     * donc si on fait une condition if($('p)) ça sera toujours vraie
+     */ 
+    // Concaténation avec l'opérateur + 
+    const msg = 'Vous avez tapé ' + e.target.value
+    if(p !== null) { // revient au même que de mettre if(p)
+      p.innerHTML = msg
+    } else {
+      const newP = document.createElement('p')
+      newP.innerHTML = msg
+      $('body').append(newP)
+    }
+  })
 })
